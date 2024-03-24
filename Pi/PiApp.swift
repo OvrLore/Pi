c35adef7504fc72acf547d3c9fa38405d8883bb2
OvrLore <lorenzoovera@gmail.com>
2024-03-24 22:23:53 +0100

    Initial Commit

---
+++ Pi/PiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiApp.swift
+//  Pi
+//
+//  Created by Lorenzo Overa on 24/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
